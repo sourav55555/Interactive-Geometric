b2297cfcab048f115d27f16c84593119f3fcd95c
sourav55555 <souravsinghassr555@gmail.com>
2023-02-18 00:51:36 +0600

    Added another 4 areas of  javascript

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// utility function for create area value element
+// utility function(1) for create area value element
 function displayArea(name,areaValue){
     const ol = document.getElementById("display-area");
 
@@ -22,13 +22,22 @@ function displayArea(name,areaValue){
 }
 
 
-// utility function for get integer value from inputs
+// utility function(2) for get numeric value from inputs
 function inputToInt(idName){
     const id = document.getElementById(idName);
     const value = parseFloat(id.value).toFixed(2);
     return value;
 }
 
+// utility function(3) for get numeric value from inner-text
+function textToInt(idName){
+    const id = document.getElementById(idName);
+    const value = parseFloat(id.innerText).toFixed(2);
+    return value;
+}
+
+
+// triangle area with utility function(1) & (2)
 document.getElementById("triangle-button").addEventListener("click", function(){
     const b = inputToInt("triangle-b");
     const h = inputToInt("triangle-h");
@@ -36,5 +45,58 @@ document.getElementById("triangle-button").addEventListener("click", function(){
     const triArea = .5*b*h;
 
     displayArea("Triangle",triArea);
+})
+
+// Rectangle area with utility function(1) & (2)
+document.getElementById("rectangle-button").addEventListener("click", function(){
+    const w = inputToInt("rectangle-w");
+    const l = inputToInt("rectangle-l");
+
+    const rectArea = w*l;
+
+    displayArea("Rectangle",rectArea);
+})
+
+// parallelogram area with utility function(1) & (3)
+document.getElementById("parallelogram-button").addEventListener("click", function(){
+    const b1 = textToInt("parallelogram-b");
+    const h1 = textToInt("parallelogram-h");
+   
+    const paraArea = b1 * h1;
+
+    displayArea("Parallelogram", paraArea);
+
+})
+
+// rhombus area with utility function(1) & (3)
+document.getElementById("rhombus-button").addEventListener("click", function(){
+    const d1 = textToInt("rhombus-d1");
+    const d2 = textToInt("rhombus-d2");
+   
+    const rhomArea = 0.5 * d1 * d2;
+    
+    displayArea("Rhombus", rhomArea);
+
+})
+
+// ellipse area with utility function(1) & (3)
+document.getElementById("ellipse-button").addEventListener("click", function(){
+    const a = textToInt("ellipse-a");
+    const b = textToInt("ellipse-b");
+   
+    const ellipseArea = 3.14 * a * b;
+    
+    displayArea("Ellipse", ellipseArea);
+
+})
+
+// ellipse area with utility function(1) & (3)
+document.getElementById("ellipse-button").addEventListener("click", function(){
+    const a = textToInt("ellipse-a");
+    const b = textToInt("ellipse-b");
+   
+    const ellipseArea = 3.14 * a * b;
+    
+    displayArea("Ellipse", ellipseArea);
 
 })
